72bad145808dd4616b766f16ea45f92707744784
nadeem-zip <nawaznadeem781@gmail.com>
2025-02-06 22:58:21 +0530

    initialized backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express=require("express");
+const mongoose=require('mongoose');
+
+const PORT=process.env.PORT || 3002;
+const uri=process.env.MONGO_URL;
+const app=express();
+app.listen(PORT,()=>{
+    console.log(`App started at port : ${PORT}`);
+    mongoose.connect(uri)
+    console.log("Databse connected");
+})
